5951514767ddad35ed0718d606aa0fc3bd63e512
Rokas <rookas.rudzenskas@gmail.com>
2022-11-13 12:29:55 +0200

    Add Pusher
    This commit adds Pusher to the project.
    
    Pusher is a hosted service that makes it super-easy to add realtime data and functionality to web and mobile applications.
    
    This commit adds the Pusher library to the project and uses it to send a message to the server when a new message is sent.

---
+++ pusher.ts
@@ -0,0 +1,14 @@
+import Pusher from "pusher";
+import ClientPusher from "pusher-js";
+
+export const serverPusher = new Pusher({
+    appId: "1506051",
+    key: "3cfabf3f0f00a0d701fb",
+    secret: "07e26743dd9c2dba0295",
+    cluster: "ap2",
+    useTLS: true,
+});
+
+export const clientPusher = new ClientPusher("3cfabf3f0f00a0d701fb", {
+    cluster: 'ap2',
+});

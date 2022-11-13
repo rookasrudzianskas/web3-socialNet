2fc138588736b681a6dd130753a56f6b921c70ad
Rokas <rookas.rudzenskas@gmail.com>
2022-11-13 13:24:15 +0200

    Add sign in button
    This commit adds a sign in button to the sign in page.

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -1,7 +1,7 @@
-import {getProviders} from "next-auth/react";
+"use client"
 
-"use-client"
 
+import {getProviders, signIn} from "next-auth/react";
 import React from 'react';
 
 type Props = {
@@ -11,7 +11,15 @@ type Props = {
 const SignInComponent = ({providers}: Props) => {
     return (
         <div>
-
+            {Object.values(providers!).map((provider, index) => (
+                <div key={index}>
+                    <button onClick={() => signIn(provider.id, {
+                        callbackUrl: (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000') + '/auth/signin',
+                    })}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
         </div>
     );
 };
